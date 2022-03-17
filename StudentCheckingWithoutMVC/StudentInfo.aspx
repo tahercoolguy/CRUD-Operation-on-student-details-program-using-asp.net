921315fbb875d5a13acb9679d04eded213b75d23
taher <tahercoolguy@gmail.com>
2022-03-17 13:03:05 +0530

    Student Details program CRUD operation

---
+++ StudentCheckingWithoutMVC/StudentInfo.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentInfo.aspx.cs" Inherits="StudentCheckingWithoutMVC.StudentInfo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+  <meta charset="utf-8">
+  <title>StudentDetails</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div style="width:100%;text-align:center">
+
+  <h1> Student Detail System</h1>
+  
+  </div>
+  
+  <div style="width:100%;align-content: center">
+   <asp:HiddenField runat="server" ID="hid_StudentID" />
+  <div style="margin:10px;display: inline-block;">Name : <asp:TextBox runat="server" ID="txtName" class="form-control"></asp:TextBox>   <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtName" ErrorMessage="Enter  Name"
+                                            CssClass="lable-Error"></asp:RequiredFieldValidator></div>
+  
+  <div style="margin:10px;display: inline-block;"> Address : <asp:TextBox runat="server" ID="txtAddress" class="form-control"></asp:TextBox>   </div>
+  
+  <div style="margin:10px;display: inline-block;"> Telephone : <asp:TextBox runat="server" ID="txtTelephone" class="form-control"></asp:TextBox></div>
+  
+  
+  
+  <div style="margin:10px;display: inline-block;"> Standard : <asp:TextBox runat="server" ID="txtStandard" class="form-control"></asp:TextBox></div>
+  
+  <div style="margin:10px;display: inline-block;" > Roll Number : <asp:TextBox runat="server" ID="txtRollNumber" class="form-control"></asp:TextBox></div>
+  
+  </div>
+  
+  <div class="w3-cell-row w3-center w3-padding">
+  
+  
+  
+  <asp:Button ID="Button1" runat="server" name="submit" Text="Insert" class="w3-btn w3-blue w3-border w3-margin" type="submit" value="Insert" OnClick="Insert_Click"/>
+  
+  <asp:Button ID="update" runat="server" name="submit" Text="Update" class="w3-btn w3-blue w3-border w3-margin" type="submit" value="Update" OnClick="update_Click"/>
+  
+  <asp:Button ID="delete" runat="server" name="submit" Text="Delete" class="w3-btn w3-blue w3-border w3-margin" type="submit" value="Delete" OnClick="delete_Click"/>
+  
+  </div>
+  
+  
+  <asp:GridView ClientIDMode="Static" ID="datatable1" runat="server" AutoGenerateColumns="False" CssClass="w3-table-all w3-small" DataKeyNames="student_id"
+                        CellPadding="0" border="0" class="datatable table table-striped table-bordered table-hover" OnRowDataBound="datatable1_RowDataBound" >
+                        <Columns>
+                            <asp:BoundField DataField="student_id" ItemStyle-CssClass="hidden" HeaderStyle-CssClass="hidden" HeaderText="Category Code" SortExpression="Customer_Cd" />
+                            <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                            <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" />
+                            <asp:BoundField DataField="Telephone" HeaderText="Telephone" SortExpression="Telephone" />
+                                <asp:BoundField DataField="Roll_Number" HeaderText="Roll Number"  />
+                                <asp:BoundField DataField="Standard" HeaderText="Standard" />
+                            
+                                                    </Columns>
+                    </asp:GridView>
+  
+ 
+    </div>
+    </form>
+</body>
+</html>
---
+++ StudentCheckingWithoutMVC/StudentInfo.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace StudentCheckingWithoutMVC
+{
+    public partial class StudentInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+                getStudentList();
+                if (!IsPostBack)
+                {
+                    if (Request.QueryString["Id"] != null)
+                    {
+                        #region "Check ID String"
+                        getRecord(Convert.ToInt32(Request.QueryString["Id"]));
+
+                        #endregion "Check Edit Permissions"
+                    }
+
+                }
+            }
+            catch (Exception lo)
+            {
+
+            }
+           
+        }
+
+        public void getRecord(int id)
+        {
+            SqlConnection con = new SqlConnection(Getconnectionstring("studentconnection"));
+            con.Open();
+           
+
+
+            SqlCommand command = new SqlCommand("Select Name,Telephone,Address,Roll_Number,Standard,student_id from student_table where student_id="+id, con);
+          
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        txtStandard.Text = reader.GetString(4);
+                        txtTelephone.Text = reader.GetString(1);
+                        txtName.Text = reader.GetString(0);
+                        txtRollNumber.Text = reader.GetString(3);
+                        txtAddress.Text = reader.GetString(2);
+                        hid_StudentID.Value = reader.GetInt32(5).ToString();
+                        //display retrieved record
+                       
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found.");
+                }
+
+            }
+
+            con.Close();
+        }
+
+        private void getStudentList()
+        {
+
+            using (SqlConnection con = new SqlConnection(Getconnectionstring("studentconnection")))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM student_table"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            datatable1.DataSource = dt;
+                            datatable1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(Getconnectionstring("studentconnection"));
+                con.Open();
+                String insert = "INSERT INTO student_table(Name,Address,Telephone,Roll_Number,Standard) VALUES ('" + txtName.Text + "','" + txtAddress.Text + "','" + txtTelephone.Text + "','" + txtRollNumber.Text + "','" + txtStandard.Text + "')";
+                SqlCommand cmd= new SqlCommand(insert,con);
+               int m = cmd.ExecuteNonQuery();  
+                if(m != 0)  
+                {  
+                   Response.Write("  <script>alert('Data Inserted !!')</script>  ");  
+                }  
+                else  
+                {  
+                   Response.Write("  <script>alert('Data Inserted !!')</script>  ");  
+                }
+                
+                con.Close();
+                
+       
+                    clearAll();
+                
+            }
+            catch (Exception e1)
+            {
+            }
+        }
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+         
+            try
+            {
+                SqlConnection con = new SqlConnection(Getconnectionstring("studentconnection"));
+                con.Open();
+                String insert = "DELETE from student_table where student_id = "+hid_StudentID.Value;
+                SqlCommand cmd= new SqlCommand(insert,con);
+               int m = cmd.ExecuteNonQuery();  
+                if(m != 0)  
+                {  
+                   Response.Write("  <script>alert('Data Deleted !!')</script>  ");  
+                }  
+                else  
+                {
+                    Response.Write("  <script>alert('Data Deleted!!')</script>  ");  
+                }
+                
+                con.Close();
+                
+       
+                    clearAll();
+                
+            }
+         
+            catch (Exception e1)
+            {
+            }
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(Getconnectionstring("studentconnection"));
+                con.Open();
+                String insert = "UPDATE student_table set Name=" + txtName.Text + ",Address=" + txtAddress.Text + ",Telephone=" + txtTelephone.Text + ",Roll_Number=" + txtRollNumber.Text + ",Standard=" + txtStandard.Text + " where student_id = " + hid_StudentID.Value;
+                SqlCommand cmd = new SqlCommand(insert, con);
+                int m = cmd.ExecuteNonQuery();
+                if (m != 0)
+                {
+                    Response.Write("  <script>alert('Data Updated !!')</script>  ");
+                }
+                else
+                {
+                    Response.Write("  <script>alert('Data Updated !!')</script>  ");
+                }
+
+                con.Close();
+
+
+                clearAll();
+
+            }
+
+            catch (Exception e1)
+            {
+            }
+        }
+
+        public void clearAll()
+        {
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtStandard.Text = "";
+            txtRollNumber.Text = "";
+            txtTelephone.Text = "";
+            Response.Redirect("StudentInfo.aspx");
+        }
+
+        protected void datatable1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Attributes["onclick"] = "window.location.replace('StudentInfo.aspx?Id=" + e.Row.Cells[0].Text + "');";
+                e.Row.Attributes["style"] = "cursor:pointer";
+            }
+        }
+
+
+        public static string Getconnectionstring(string keyname)
+        {
+            string connection = string.Empty;
+            switch (keyname)
+            {
+                case "studentconnection":
+                    connection = ConfigurationManager.ConnectionStrings["studentconnection"].ConnectionString;
+                    break;
+              
+                default:
+                    break;
+            }
+            return connection;
+
+        } 
+
+    }
+}
